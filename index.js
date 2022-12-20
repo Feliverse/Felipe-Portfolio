c3e7d12b7d3d867be6f3c187951e8106498877ab
Feliverse <114632980+Feliverse@users.noreply.github.com>
2022-12-20 14:39:32 -0400

    adjunsting background menu

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const menuContainer = document.getElementById('toolbar');
-
+const iconImage = menuContainer.querySelector("img");
 let controller = false;
 menuContainer.addEventListener('click', () => {
-  const iconImage = menuContainer.querySelector("img");
   if (!controller) {
     document.querySelector('.desk').style.display = 'flex';
     document.querySelector('.desk').style.background = '#fff';
     document.querySelector('.desk').style.position = 'absolute';
     document.querySelector('.desk').style.width = '100%';
-    document.querySelector('.desk').style.height = '100%';
-    document.querySelector('.desk').style.top = '40px';
+    document.querySelector('.desk').style.height = '100vh';
+    document.querySelector('.desk').style.top = '0px';
     document.querySelector('.desk').style.position = 'fixed';
 
-    //document.getElementById('toolbar').style
+    document.getElementById('toolbar').style.zIndex = '2';
 
     document.getElementById('top-img').style.background = 'url("images/menu_mobile/image_geometry_menu_1.jpg")';
     document.getElementById('top-img').style.position = 'absolute';
     document.getElementById('top-img').style.width = '70vw';
     document.getElementById('top-img').style.height = '30vh';
-    document.getElementById('top-img').style.top = '-40px';
+    document.getElementById('top-img').style.top = '0px';
     document.getElementById('top-img').style.left = '0';
     document.getElementById('top-img').style.backgroundSize = '100%';
     document.getElementById('top-img').style.backgroundRepeat = 'no-repeat';
@@ -34,7 +33,6 @@ menuContainer.addEventListener('click', () => {
     document.querySelector('.desk ul').style.listStyle = 'none';
     document.querySelector('.desk ul').style.display = 'flex';
     document.querySelector('.desk ul').style.flexDirection = 'column';
-    //document.querySelector('.desk ul').style.justifyContent = 'center';
     document.querySelector('.desk ul').style.alignItems = 'center';
     document.querySelectorAll('.menu a').forEach((item) => {
       item.style.textDecoration = 'none';
@@ -49,7 +47,7 @@ menuContainer.addEventListener('click', () => {
       item.style.marginBottom = '30px';
     });
 
-    document.querySelector('.desk').style.paddingTop = '150px';
+    document.querySelector('.desk').style.paddingTop = '50%';
     document.querySelector('.desk').style.justifyContent = 'center';
     iconImage.src = 'images/cancel.jpg';
 
@@ -62,11 +60,12 @@ menuContainer.addEventListener('click', () => {
 });
 
 const cancel = document.querySelector('.menu');
-
 const cancela = cancel.querySelectorAll('a');
 
 cancela.forEach((a) => {
   a.addEventListener('click', () => {
+    controller = false;
     document.querySelector('.desk').style.display = 'none';
+    iconImage.src = 'images/ic_menu.svg';
   });
 });

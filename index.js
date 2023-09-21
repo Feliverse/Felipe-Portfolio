949a0f3db53a3684f001f3c59d777e7d7e7e3e24
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-09-20 20:43:38 -0400

    mobile nav ready

--- index.js
+++ index.js
@@ -1,75 +1,69 @@
-// mobile menu
 
-const menuContainer = document.getElementById('toolbar');
-const iconImage = menuContainer.querySelector('img');
-let controller = false;
+const mobileNavButton = document.getElementById('mobile-nav-button');
+const mobileMenu = document.getElementById('mobile-menu');
+const topImg = document.getElementById('top-img');
+const bottomImg = document.getElementById('bottom-img');
+const nav = document.querySelector('nav');
 
-menuContainer.addEventListener('click', () => {
-  if (!controller) {
-    document.querySelector('.desk').style.display = 'flex';
-    document.querySelector('.desk').style.background = '#fff';
-    document.querySelector('.desk').style.position = 'absolute';
-    document.querySelector('.desk').style.width = '100%';
-    document.querySelector('.desk').style.height = '100vh';
-    document.querySelector('.desk').style.top = '0px';
-    document.querySelector('.desk').style.position = 'fixed';
+mobileNavButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    if (!mobileMenu.classList.contains('hidden')) {
+        mobileMenu.style.display = 'grid';
+        mobileMenu.style.background = '#fff';
+        mobileMenu.style.justifyItems = 'center';
+        mobileMenu.style.width = '110%';
+        mobileMenu.style.height = '93vh';
+        mobileMenu.style.alignItems = 'center';
+        mobileMenu.style.alignContent = 'center';
+        mobileMenu.style.gap = '3rem';
 
-    document.getElementById('toolbar').style.zIndex = '2';
+        // Mostrar las imágenes cuando se abre el menú
+        topImg.style.background = 'url("images/menu_mobile/image_geometry_menu_1.jpg")';
+        topImg.style.display = 'block';
+        topImg.style.position = 'absolute';
+        topImg.style.width = '70vw';
+        topImg.style.height = '30vh';
+        topImg.style.top = '0px';
+        topImg.style.left = '0';
+        topImg.style.backgroundSize = '100%';
+        topImg.style.backgroundRepeat = 'no-repeat';
 
-    document.getElementById('top-img').style.background = 'url("images/menu_mobile/image_geometry_menu_1.jpg")';
-    document.getElementById('top-img').style.position = 'absolute';
-    document.getElementById('top-img').style.width = '70vw';
-    document.getElementById('top-img').style.height = '30vh';
-    document.getElementById('top-img').style.top = '0px';
-    document.getElementById('top-img').style.left = '0';
-    document.getElementById('top-img').style.backgroundSize = '100%';
-    document.getElementById('top-img').style.backgroundRepeat = 'no-repeat';
+        bottomImg.style.background = 'url("images/menu_mobile/image_geometry_menu_2.jpg")';
+        bottomImg.style.display = 'block';
+        bottomImg.style.position = 'absolute';
+        bottomImg.style.width = '100vw';
+        bottomImg.style.height = '220px';
+        bottomImg.style.bottom = '0';
+        bottomImg.style.backgroundSize = '100%';
+        bottomImg.style.backgroundRepeat = 'no-repeat';
+        nav.style.zIndex = '100';
 
-    document.getElementById('bottom-img').style.background = 'url("images/menu_mobile/image_geometry_menu_2.jpg")';
-    document.getElementById('bottom-img').style.position = 'absolute';
-    document.getElementById('bottom-img').style.width = '100vw';
-    document.getElementById('bottom-img').style.height = '220px';
-    document.getElementById('bottom-img').style.bottom = '0';
-    document.getElementById('bottom-img').style.backgroundSize = '100%';
-    document.getElementById('bottom-img').style.backgroundRepeat = 'no-repeat';
+        nav.style.backgroundColor = 'white';
+    } else {
+        mobileMenu.style.backgroundColor = '';
+        mobileMenu.style.display = 'none';
 
-    document.querySelector('.desk ul').style.listStyle = 'none';
-    document.querySelector('.desk ul').style.display = 'flex';
-    document.querySelector('.desk ul').style.flexDirection = 'column';
-    document.querySelector('.desk ul').style.alignItems = 'center';
-    document.querySelectorAll('.menu a').forEach((item) => {
-      item.style.textDecoration = 'none';
-      item.style.color = 'black';
-      item.style.fontWeight = '400px';
-      item.style.size = '2rem';
-      item.style.fontFamily = 'Poppins';
-      item.style.justifyself = 'center';
-    });
-
-    document.querySelectorAll('.menu li').forEach((item) => {
-      item.style.marginBottom = '4rem';
-    });
-
-    document.querySelector('.desk').style.paddingTop = '50%';
-    document.querySelector('.desk').style.justifyContent = 'center';
-    iconImage.src = 'images/cancel.jpg';
-
-    controller = true;
-  } else {
-    iconImage.src = 'images/ic_menu.svg';
-    document.querySelector('.desk').style.display = 'none';
-    controller = false;
-  }
+        // Ocultar las imágenes cuando se cierra el menú
+        topImg.style.display = 'none';
+        bottomImg.style.display = 'none';
+        nav.style.backgroundColor = '';
+    }
 });
 
-const cancel = document.querySelector('.menu');
-const cancela = cancel.querySelectorAll('a');
+const closeMobileMenu = () => {
+  mobileMenu.classList.add('hidden');
+  mobileMenu.style.backgroundColor = '';
+  topImg.style.display = 'none';
+  bottomImg.style.display = 'none';
+  nav.style.backgroundColor = '';
+};
 
-cancela.forEach((a) => {
-  a.addEventListener('click', () => {
-    controller = false;
-    document.querySelector('.desk').style.display = 'none';
-    iconImage.src = 'images/ic_menu.svg';
+const menuLinks = mobileMenu.querySelectorAll('a');
+
+menuLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    // Cierra el menú al hacer clic en un enlace
+    closeMobileMenu();
   });
 });
 
